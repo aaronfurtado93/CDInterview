b035690458da20fb7373afa437ec0f613f6c8846
Aaron Furtado <aaronfurtado93@hotmail.com>
2015-07-15 09:38:10 +0530

    asset changes

--- www/js/libs/database.utilities.js
+++ www/js/libs/database.utilities.js
@@ -20,6 +20,10 @@ function createRestaurantTable(tx) {
     query += "distance Numeric(3)";
     query += ")"; //endColumnsDefinition
     tx.executeSql(query);
+    
+    query = "truncate table "; //truncateTable
+    query += "temp_Restaurant_Table"; // tablename
+    tx.executeSql(query);
 }
 
 function createRestaurantTableFailed (error) {
@@ -39,7 +43,41 @@ function createRestaurantTableIfNotExists ()
 
 //====================================================================================================
 
+function insertRestaurantTableRow(tx) {
+    var query = "create table if not exists "; //createIfNotExistsStatement
+    query += "temp_Restaurant_Table"; // tablename
+    query += " ("; //startColumnsDefinition
+    query += "restaurantDataID Numeric(4) Primary Key,";
+    query += "restaurantName Varchar(50),";
+    query += "logoURL Varchar(500),";
+    query += "offers Numeric(10),";
+    query += "cuisine Varchar(5000),";
+    query += "neighbourhoodName Varchar(50),";
+    query += "latitude Numeric(4,6),";
+    query += "longitude Numeric(4,6),";
+    query += "distance Numeric(3)";
+    query += ")"; //endColumnsDefinition
+    tx.executeSql(query);
+    
+    query = "truncate table "; //truncateTable
+    query += "temp_Restaurant_Table"; // tablename
+    tx.executeSql(query);
+}
+
+function insertRestaurantTableRowFailed (error) {
+    console.error("insertRestaurantTableRow Failed");
+    console.error("Error message: " + error);
+}
+
+function insertRestaurantTableRowSucceeded () {
+    console.info("insertRestaurantTableRow Succeeded");
+}
+
+//====================================================================================================
+
 function insertValuesIntoRestaurantTable (restaurantData) {
+    
+    databaseObject = accessDatabase();
     
     $.each( restaurantData, function( restaurantDataID, restaurantDataValues ) {
       /*console.log( restaurantDataID + ": " + restaurantDataValues );*/
@@ -53,14 +91,30 @@ function insertValuesIntoRestaurantTable (restaurantData) {
         });
         cuisine = cuisine.substring(0,cuisine.lastIndexOf(","));
         cuisine += "]}"
-        debugger;
         var neighbourhoodName = restaurantDataValues.NeighbourhoodName;
         var latitude = restaurantDataValues.Latitude;
         var longitude = restaurantDataValues.Longitude;
         var distance = calculateDistanceFromCurrentLocation(latitude, longitude);
+        
+        var restaurantRowData = {
+            restaurantDataID: restaurantDataID,
+            restaurantName: restaurantName,
+            logoURL: logoURL,
+            offers: offers,
+            cuisine: cuisine,
+            neighbourhoodName: neighbourhoodName,
+            latitude: latitude,
+            longitude: longitude,
+            distance: distance
+        }
+        
+        console.log(restaurantRowData);
+        
         debugger;
+        
+        databaseObject.transaction(insertRestaurantTableRow, insertRestaurantTableRowFailed, insertRestaurantTableRowSucceeded);
     });
     debugger;
-    databaseObject = accessDatabase();
+    
 }
 
